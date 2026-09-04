5145564eb8af03e63966cadc6bb3aa8bfb3978fa
aisuski <uwaifoaisosa18@gmail.com>
2026-09-04 17:42:00 +0100

    fix: prevent duplicate history entries, auto-reset form after successful split

--- arc-splitter/src/stubs/empty.ts
+++
@@ -1,9 +0,0 @@
-export default {};
-export const createTransactionMessage = () => ({});
-export const setTransactionMessageFeePayerSigner = () => ({});
-export const setTransactionMessageLifetimeUsingBlockhash = () => ({});
-export const pipe = () => ({});
-export const getTransferSolInstruction = () => ({});
-export const findAssociatedTokenPda = () => ({});
-export const getCreateAssociatedTokenIdempotentInstruction = () => ({});
-export const getTransferInstruction = () => ({});
